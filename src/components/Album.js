c9c6f48eae4cf2499101b86860eb679ac667292d
AshleeKiel <ashleekiel@hotmail.com>
2019-04-04 14:29:15 -0400

    assignment-13 player bar Next button click added

--- src/components/Album.js
+++ src/components/Album.js
@@ -54,6 +54,14 @@ class Album extends Component {
         this.play();
     }
 
+    handleNextClick() {
+        const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+        const newIndex = Math.min(4, currentIndex + 1);
+        const newSong = this.state.album.songs[newIndex];
+        this.setSong(newSong);
+        this.play();
+    }
+
     handleMouseEnter(song) {
         this.setState( {isHovered : song} );
     }
@@ -107,6 +115,7 @@ class Album extends Component {
                 currentSong={this.state.currentSong}
                 handleSongClick={() => this.handleSongClick(this.state.currentSong)} 
                 handlePrevClick={() => this.handlePrevClick()}
+                handleNextClick={() => this.handleNextClick()}
                 />
             </section>
         );
